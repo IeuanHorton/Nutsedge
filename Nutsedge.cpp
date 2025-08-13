3599370cb1954d42c8235508a8864cd39c34f753
IeuanHorton <HortonIeuan@gmail.com>
2025-08-13 01:29:30 +0000

    added method to replace word in a phrase

--- Nutsedge.cpp
+++ Nutsedge.cpp
@@ -1,10 +1,25 @@
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-class Nutsedge() {
-	
-}
+class Nutsedge {
+
+	public:
+	std::string ReplaceWord(std::string phrase, std::string badWord, std::string goodWord)
+	{
+		int index = phrase.find(badWord);
+		int length = badWord.length();
+		
+		phrase.erase(index, length);
+		phrase.insert(index, goodWord);
+
+		return phrase;
+	}	
+};
 
 int main(){
 	Nutsedge nutsedge;
+	std::cout << nutsedge.ReplaceWord("The toad prince", "toad", "frog") << std::endl;
 	return 0;
 }
